78311ed3f31076f6724e7a4db88244e6c132dc46
andriiglukhyi <andrigluxii@gmail.com>
2018-06-20 17:14:26 -0700

    new kata

---
+++ peak_index/solution.py
@@ -0,0 +1,11 @@
+class Solution:
+    def peakIndexInMountainArray(self, A):
+        """
+        :type A: List[int]
+        :rtype: int
+        """
+        for item in range(len(A)):
+            print(item)
+            if A[item]>A[item+1] and A[item] > A[item-1]:
+                return item
+            
