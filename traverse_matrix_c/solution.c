39626d582d018ff2e40117f788d07a6afaef0027
andriiglukhyi <andrigluxii@gmail.com>
2018-06-26 12:44:34 -0700

    c traverse matrix

---
+++ traverse_matrix_c/solution.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#define MAX 10000
+
+
+void rowMajor(int a,int b) {
+ 
+  int i, j;
+ 
+  // accessing element row wise
+  for (i = 0; i < a; i++) {
+    for (j = 0; j < b; j++) {
+        if (arr[i][j] != 0){
+                       
+        }  
+      arr[i][j]++;
+    }
+  }
+}
+
