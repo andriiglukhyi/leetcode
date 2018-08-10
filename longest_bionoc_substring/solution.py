b4e0d5ff1b36781e5aa91009f88f071844f4100f
andriiglukhyi <andriigluxii@gmail.com>
2018-08-10 13:22:02 -0700

    bionic substring

---
+++ longest_bionoc_substring/solution.py
@@ -0,0 +1,48 @@
+# Dynamic Programming implementation of longest bitonic subsequence problem
+"""
+lbs() returns the length of the Longest Bitonic Subsequence in
+arr[] of size n. The function mainly creates two temporary arrays
+lis[] and lds[] and returns the maximum lis[i] + lds[i] - 1.
+ 
+lis[i] ==> Longest Increasing subsequence ending with arr[i]
+lds[i] ==> Longest decreasing subsequence starting with arr[i]
+"""
+ 
+def lbs(arr):
+    n = len(arr)
+ 
+ 
+    # allocate memory for LIS[] and initialize LIS values as 1
+    # for minus
+    lis = [0 for i in range(n)-2]
+ 
+    # Compute LIS values from left to right
+    for i in range(0 , n-1):
+        if arr[i] > arr[i+1]:
+            lis[i] = 1
+        else:
+            lis[i] = -1
+ 
+    # allocate memory for LDS and initialize LDS values for
+    # all indexes
+    ans = []
+     
+    # Compute LDS values from right to left
+    counter = 0
+    for i in range(len(lis)):
+        if arr[i] == ar[i+1]:
+            counter += 1
+        else:
+            ans.append(counter + 1)
+     
+    biggest = 0
+    # Return the maximum value of (lis[i] + lds[i] - 1)
+    for i in range(len(ans)-1):
+        if ans[i] + ans[i+1] > biggest:
+            biggest = ans[i] + ans[i+1]
+     
+    return biggest
+# Driver program to test the above function
+arr =  [0 , 8 , 4, 12, 2, 10 , 6 , 14 , 1 , 9 , 5 , 13,
+        3, 11 , 7 , 15]
+lbs(arr)
