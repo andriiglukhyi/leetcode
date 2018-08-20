3b2294027d56d1dc6618a83ec5f2ec396fe94533
andriiglukhyi <andriigluxii@gmail.com>
2018-08-20 10:16:06 -0700

    harmonius subsiquence

---
+++ longest_harmonius_subsequence/solution.py
@@ -0,0 +1,20 @@
+class Solution(object):
+    def findLHS(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: int
+        """
+        longest = 0
+        dict = {}
+        for number in nums:
+            if number in dict:
+                dict[number] += 1
+            else:
+                dict[number] = 1
+        for item in range(len(nums)-1):
+            cur = dict[nums[item]]
+            if nums[item] + 1 in dict:
+                cur += dict[nums[item]+1]
+            if cur > longest:
+                longest = cur
+        return longest
