b80d24534f1fef24fe6727017d9126134e0d3961
andriiglukhyi <andriigluxii@gmail.com>
2018-08-27 10:04:00 -0700

    power of tree

---
+++ power_of_tree/solution.py
@@ -0,0 +1,9 @@
+class Solution:
+    def isPowerOfThree(self, n):
+        """
+        :type n: int
+        :rtype: bool
+        """
+        if n <= 0:
+            return False
+        return 1162261467 % abs(n) == 0
