708cbea73ec4c2ef3d48c3fcd4e66887866649aa
andriiglukhyi <andriigluxii@gmail.com>
2018-09-11 12:19:48 -0700

    new fix for licence key

--- license_key_formatting/solution.py
+++ license_key_formatting/solution.py
@@ -10,12 +10,17 @@ class Solution(object):
             if item.isalpha() or item.isnumeric():
                 end += item.upper()
         end1 = ''
-        counter = 1
+        counter = 0
         for item in reversed(range(len(end))):
-            counter += 1
             if counter == K:
                 end1 += "-" + end[item]
-                counter = 1
+                counter = 0
             else:
                 end1+= end[item]
-            print(end1)
+            counter += 1
+        print(end1[::-1])
+
+
+inst = Solution()
+print(inst.licenseKeyFormatting("5F3Z-2e-9-w",4))
+print(inst.licenseKeyFormatting("2-5g-3-J",2))
